3c5b1ec378f10ca5eff82508ef2593852534d6c1
ShaziaYousaf <140582718+ShaziaYousaf@users.noreply.github.com>
2023-08-12 07:00:52 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,130 @@
+var message:string = "hey I am Abu HurairA";
+console.log(message)
+/*
+var userName:string = "shazia Yousaf";
+console.log(message,userName)
+var num1:number = 20;
+var num2:number = 1;
+var num3:number = 10;
+var num4:number = 30;
+var num5:number = 5;
+var num6:number = 10;
+var num7:number = 25;
+var num8:number = 44;
+var num9:number = 97;
+var num10:number = 30;
+
+var result:number=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10; 
+console.log(result)*/
+
+/*var num1:number = 20;
+var num2:number = 1;
+var num3:number = 10;
+var num4:number = 30;
+var num5:number = 5;
+var num6:number = 10;
+var num7:number = 25;
+var num8:number = 44;
+var num9:number = 97;
+var num10:number = 30;
+var result:number=num1-num2-num3-num4-num5-num6-num7-num8-num9-num10;
+console.log(result)
+
+
+var num1:number = 20;
+var num2:number = 1;
+var num3:number = 10;
+var num4:number = 30;
+var num5:number = 5;
+var num6:number = 10;
+var num7:number = 25;
+var num8:number = 44;
+var num9:number = 97;
+var num10:number = 30;
+var result:number=num1*num2*num3*num4*num5*num6*num7*num8*num9*num10;
+console.log(result)
+
+
+var num1:number = 20;
+var num2:number = 1;
+var num3:number = 10;
+var num4:number = 30;
+var num5:number = 5;
+var num6:number = 10;
+var num7:number = 25;
+var num8:number = 44;
+var num9:number = 97;
+var num10:number = 30;
+var result:number=num1/num2/num3/num4/num5/num6/num7/num8/num9/num10;
+console.log(result)
+
+var num9:number = 97;
+var num10:number = 30;
+var result:number=num9%num10;
+console.log(result)*/
+
+
+/*var num4:number = 30;
+var num6:number = 10;
+var num7:number = 25;
+var num8:number = 44;
+var num9:number = 97;
+var num10:number = 30;
+var result:number=num4+num6-num7*num8/num9%num10;
+console.log(result)
+
+//to find are of rectangle
+var length:number = 15;
+var width:number = 20;
+var output=length*width;
+console.log(length*width)
+
+//to find the volume of a cube
+var a:number=3;
+var area=a*a*a;
+console.log(area)*/
+
+//program that shows that a given number is positive ,negative or zero
+
+var number:number=-2;
+
+if(number > 0){
+    console.log("the number is positive")
+}
+
+else if(number<0){
+    console.log("the is negative")
+}
+
+else{
+    console.log("the number is zeo")
+}
+
+//write a programe that shows given number is even or odd
+var number1:number=5;
+if(number==2){console.log("the number is even")
+
+}
+
+else{
+    console.log("the number is odd")
+}
+//write a programe that determines if a person is eligible to vote based on their age
+var validAge:number=15;
+if(validAge==18){
+    console.log("person is eligible")
+}
+else{
+    console.log("the person is not eligible")
+
+}
+
+ var a1:number=10;
+ var a2:number=5;
+ var a3:number=3;
+ var a4:number=2;
+ var a5:number=4;
+ var a6:number=7;
+
+var response=(((a1+a2)*a3-a4)/(a5%a3)-a6)
+console.log(response)
